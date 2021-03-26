eb0a385dabc0dc7bafa31c7d55cdbdcf3dc7b473
Muhammad Farras Jibran <50952131+farasjibran@users.noreply.github.com>
2021-03-26 11:04:40 +0700

    update in laporan. add table in laporan

--- src/Screens/LaporanScreen.js
+++ src/Screens/LaporanScreen.js
@@ -1,6 +1,122 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { MDBDataTable } from 'mdbreact'
+import MySwal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 function LaporanScreen() {
+  const API_URL = `http://localhost:8000/`
+  const Swal = withReactContent(MySwal)
+  const [tableLaporan, setTableLaporan] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  // Setting the data table
+  const datatableLaporan = (laporan) => {
+    let rowsData = []
+
+    for (var index = 0; index < laporan.length; index++) {
+      let rowItem = {}
+      rowItem['no'] = index + 1
+      rowItem['nik'] = laporan[index].nik
+      rowItem['tgl_pengaduan'] = laporan[index].tgl_pengaduan
+      rowItem['isi_laporan'] = laporan[index].isi_laporan
+      rowItem['foto_laporan'] = (
+        <img
+          style={{ width: '200px', height: '150px', borderRadius: '5px' }}
+          src={laporan[index].foto_laporan}
+          alt="tidak ada gambar"
+        />
+      )
+      rowItem['status'] = (
+        <div class="badge badge-success badge-pill">
+          {laporan[index].status}
+        </div>
+      )
+      rowItem['action'] = (
+        <>
+          <button
+            class="btn btn-datatable btn-icon btn-transparent-dark"
+            id={laporan[index].id}
+          >
+            <i className="fas fa-ellipsis-v" />
+          </button>
+          <button
+            class="btn btn-datatable btn-icon btn-transparent-dark mr-2"
+            id={laporan[index].id}
+          >
+            <i className="far fa-trash-alt" />
+          </button>
+        </>
+      )
+      rowsData.push(rowItem)
+    }
+    setTableLaporan(rowsData)
+  }
+
+  //   fetch laporan data
+  const fetchLaporan = async () => {
+    try {
+      const data = await fetch(`${API_URL}api/getlaporan`, {
+        method: 'GET',
+      })
+      const laporandata = await data.json()
+      datatableLaporan(laporandata.result)
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+  }
+
+  //   running the state
+  useEffect(() => {
+    fetchLaporan().then(() => {
+      setLoading(true)
+    })
+  }, [])
+
+  // data laporan
+  const dataLaporan = (data) => {
+    return {
+      columns: [
+        {
+          label: 'No',
+          field: 'no',
+          sort: 'asc',
+        },
+        {
+          label: 'NIK',
+          field: 'nik',
+          sort: 'asc',
+        },
+        {
+          label: 'Tanggal Pengaduan',
+          field: 'tgl_pengaduan',
+          sort: 'asc',
+        },
+        {
+          label: 'Isi Laporan',
+          field: 'isi_laporan',
+          sort: 'asc',
+        },
+        {
+          label: 'Foto Laporan',
+          field: 'foto_laporan',
+          sort: 'asc',
+        },
+        {
+          label: 'Status',
+          field: 'status',
+          sort: 'asc',
+        },
+        {
+          label: 'Action',
+          field: 'action',
+          sort: 'asc',
+        },
+      ],
+      rows: data,
+    }
+  }
+
   return (
     <>
       <header className="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
@@ -45,9 +161,13 @@ function LaporanScreen() {
             </div>
           </div>
           <div className="card-body">
-            The Bootstrap breadcrumbs component is supported within the page
-            header for the SB Admin Pro theme. The page header on this page is
-            an example of a page with breadcrumbs added!
+            <MDBDataTable
+              sortable={false}
+              noBottomColumns={true}
+              striped
+              data={dataLaporan(tableLaporan)}
+              responsive={true}
+            />
           </div>
         </div>
       </div>
